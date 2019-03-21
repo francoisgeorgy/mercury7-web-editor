4e5f919ef41666b1af94d57ccb932f1e3769cf51
François Georgy <francois.georgy@gmail.com>
2019-03-21 17:51:18 +0100

    adjust popup dimensions

--- src/ui_dialogs.js
+++ src/ui_dialogs.js
@@ -15,6 +15,6 @@ export function printPreset() {
     log("printPreset");
     // let url = "print.html?" + URL_PARAM_SYSEX + "=" + encodeURIComponent(LZString.compressToBase64(Utils.toHexString(MODEL.getSysEx())));
     let url = "print.html?" + URL_PARAM_SYSEX + "=" + encodeURIComponent(Utils.toHexString(MODEL.getPreset()));
-    window.open(url, "_blank", "width=700,height=500,top=100,left=200,location,resizable,scrollbars,status");
+    window.open(url, "_blank", "width=800,height=480,top=100,left=200,location,resizable,scrollbars,status");
     return false;   // disable the normal href behavior when called from an onclick event
 }
