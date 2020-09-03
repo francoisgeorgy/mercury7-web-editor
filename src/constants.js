4be4632d4e3848b1de5f8f746df5965489d4edaf
François Georgy <francois.georgy@gmail.com>
2020-09-03 22:18:09 +0200

    query string param name for local storage clearing

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,4 @@ export const URL_PARAM_SYSEX = "sysex";     // name of sysex parameter in the qu
 export const URL_PARAM_SIZE = "size";       //
 export const URL_PARAM_MARGINS = "m";        //
 export const URL_PARAM_BG = "bg";           // body background-color
+export const URL_PARAM_CLEAR_STORAGE = "deletedata";           // clear local storage
